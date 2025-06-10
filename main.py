0541fd2e4f51b852500ab21b44ab1e68a81c765e
BrunaVilas <brunavilasboas.ferreira99@gmail.com>
2025-06-09 20:45:53 -0400

    Feat captura

--- main.py
+++ main.py
@@ -8,13 +8,17 @@ import os
 """
 
 
-
-
-
-
-
-
-
+def captura_input_menu(max_opcao):
+    while True:
+        try:
+            captura = int(input("\nDigite a opção desejada: "))
+            if 0 <= captura <= max_opcao:
+                print("\n") # Adiciona espaço após input válido
+                return captura
+            else:
+                print(f"Opção inválida. Digite um número entre 0 e {max_opcao}.")
+        except ValueError:
+            print("Entrada inválida. Por favor, digite um número.")
 
 def menuOpcoes():
     print(" \n --- Sistema de Gestão de Oficinas ---")
@@ -31,3 +35,9 @@ def menuOpcoes():
     print("\n11. Ver Alunos Inscritos em uma Oficina")
     print("\n12. Ver Frequência de um Aluno em uma Oficina")
     print("\n0. Sair")
+    print("="*22)
+    return captura_input_menu(12)
+
+
+
+
