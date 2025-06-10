a35e456210f8baceb0868bd1ca7f2f366c173657
caioalonsoferreira <alonso.caioferreira@gmail.com>
2025-06-09 20:53:29 -0400

    criei os parametros

--- modelos/professor.py
+++ modelos/professor.py
@@ -1,2 +1,10 @@
-print("teste")
-print("devolvendo o teste!")
+class professor:
+
+    def __init__(self,nome,email,cpf,login,senha):
+
+        self.nome=nome
+        self.email=email
+        self.cpf=cpf
+        self.login=login
+        self.senha=senha
+    
