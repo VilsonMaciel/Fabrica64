2f2a59bd8f1ee36143e50b393f82ad99ef47ea51
ThaisMoya <thaismoya.b@hotmail.com>
2025-06-09 20:01:32 -0400

    Feature Criei o menu de opções

---
+++ main.py
@@ -0,0 +1,25 @@
+
+
+
+
+
+
+
+
+
+
+def menuOpcoes():
+    print(" \n --- Sistema de Gestão de Oficinas ---")
+    print("\n1. Cadastrar Professor")
+    print("\n2. Cadastrar alunos")
+    print("\n3. Criar Oficina")
+    print("\n4. Inscrever Aluno em Oficina")
+    print("\n5. Associar Professor à Oficina")
+    print("\n6. Registrar Frequência de Aluno")
+    print("\n7. Listar Professor")
+    print("\n8. Listar Alunos")
+    print("\n9. Listar Oficinas")
+    print("\n10. Ver Oficinas de um Professor")
+    print("\n11. Ver Alunos Inscritos em uma Oficina")
+    print("\n12. Ver Frequência de um Aluno em uma Oficina")
+    print("\n0. Sair")
