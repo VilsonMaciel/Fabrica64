4d770895a282976d852a354962d9d7244a4407ac
Leonardo Almeida Muraro <leonardo.a.comercial@gmail.com>
2025-06-10 21:44:18 -0400

    Feature: Criação da classe Aluno

--- modelos/Aluno.py
+++ modelos/Aluno.py
@@ -1,7 +1,13 @@
-import Pessoa from pessoa
-class Aluno:
+from pessoa import Pessoa
+
+class Aluno(Pessoa):
     def __init__(self,nome, cpf, email, data_nasc, telefone, genero, matricula, oficinas_inscritas, frequencia):
         super().__init__(self, nome, cpf, email, data_nasc, telefone, genero)
         self.matricula = matricula
         self.oficinas_inscritas = []
         self.frequencia = {}
+        return
+    
+    def cadastrar_aluno(self):
+        
+        
