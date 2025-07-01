13727b310466bd361bfb2aa84d73dd285db09e15
KAUANF2025 <alonso.kauanferreira@gmail.com>
2025-06-30 21:23:42 -0400

    Revert "Feature/smgo 12"

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
-from modelos.pessoa import Pessoa
-from modelos.usuario import Usuario
-from modelos.professor import Professor
-from modelos.aluno import Aluno
-from modelos.oficina import Oficina
-import json
+"""
+import modelos/pessoa.py
+import modelos/usuario.py
+import modelos/aluno.py
+import modelos/professor.py
+import modelos/oficina.py
 import os
+"""
 
 
 def captura_input_menu(max_opcao):
@@ -33,11 +34,9 @@ def menuOpcoes():
     print("\n10. Ver Oficinas de um Professor")
     print("\n11. Ver Alunos Inscritos em uma Oficina")
     print("\n12. Ver Frequência de um Aluno em uma Oficina")
-    print("\n13. Registrar dúvida")
-    print("\n14. Dúvidas frequentes")
     print("\n0. Sair")
     print("="*22)
-    return captura_input_menu(14)
+    return captura_input_menu(12)
 
 if __name__ == "__main__":
     while True:
@@ -53,15 +52,15 @@ if __name__ == "__main__":
 
         elif opcao_digitada == 3:
             os.system("cls")
-            cadastrar_oficina()
+            criar_oficina()
 
         elif opcao_digitada == 4:
             os.system("cls")
-            Oficina.adicionar_aluno()
+            inscrever_aluno_em_oficina()
 
         elif opcao_digitada == 5:
             os.system("cls")
-            Oficina.associar_professor()
+            associar_professor_a_oficina()
 
         elif opcao_digitada == 6:
             os.system("cls")
@@ -77,7 +76,7 @@ if __name__ == "__main__":
 
         elif opcao_digitada == 9:
             os.system("cls")
-            Professor.listar_oficinas()
+            listar_oficinas()
 
         elif opcao_digitada == 10:
             os.system("cls")
@@ -85,20 +84,11 @@ if __name__ == "__main__":
 
         elif opcao_digitada == 11:
             os.system("cls")
-            Oficina.get_alunos_inscritos()
+            ver_alunos_inscritos_oficina()
 
         elif opcao_digitada == 12:
             os.system("cls")
-            ver_alunos_frequencia_oficina()
-
-        elif opcao_digitada == 13:
-            os.system("cls")
-            add_duvida()
-
-        elif opcao_digitada == 14:
-            os.system("cls")
-            duvidas_frequentes()
-
+            ver_alunos_inscritos_oficina()
 
         elif opcao_digitada == 0:
             os.system("cls")
--- modelos/professor.py
+++ modelos/professor.py
@@ -1,53 +1,10 @@
-from .usuario import Usuario
-from .tipo_usuario import TipoUsuario
+class professor:
 
-class Professor(Usuario):
-    def __init__(self, nome, cpf, email, data_nasc, telefone, genero, senha, especialidade):
-        super().__init__(nome, cpf, email, data_nasc, telefone, genero, senha, TipoUsuario.professor)
+    def __init__(self,nome,email,cpf,login,senha):
 
-        if not especialidade or not isinstance(especialidade, str):
-            raise ValueError("Especialidade não pode estar vazia.")
-        
-        self._especialidade = especialidade
-        self._oficinas = []
-
-    # Getter e Setter usando @property
-    @property
-    def especialidade(self):
-        return self._especialidade
-
-    @especialidade.setter
-    def especialidade(self, nova_especialidade):
-        if not nova_especialidade or not isinstance(nova_especialidade, str):
-            raise ValueError("Nova especialidade não pode estar vazia.")
-        self._especialidade = nova_especialidade
-
-
-#métodos relacionados a oficinas 
-        
-    def adicionar_oficina_professor(self,oficina):
-
-        if oficina not in self._oficinas:
-            self._oficinas.append(oficina)
-            if hasattr(oficina, "associar_professor"):
-                oficina.associar_professor(self)
-
-
-  
-    def listar_oficinas_professor(self):
-        return self.__oficinas
-
-    # Representação do professor
-    def __str__(self):
-        return (
-            f"Professor: {self.login['email']} | Nome: {self.nome} | "
-            f"Especialidade: {self._especialidade} | "
-            f"Total de oficinas: {len(self._oficinas)}"
-        )
-
-
-
-
-
-
-      
+        self.nome=nome
+        self.email=email
+        self.cpf=cpf
+        self.login=login
+        self.senha=senha
+    
