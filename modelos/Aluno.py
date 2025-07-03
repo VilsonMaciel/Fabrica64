93644694781a6b6bb4d6de876cbf5804fc81a89a
Leonardo Almeida Muraro <leonardo.a.comercial@gmail.com>
2025-07-03 19:55:42 -0400

    FIx: Nome do arquivo para minúsculo

--- modelos/Aluno.py
+++ modelos/Aluno.py
@@ -197,5 +197,6 @@ class Aluno(Pessoa):
             except ValueError:
                 print("Entrada inválida. Por favor, digite apenas o número.")
 
-    def _editar_informacoes_aluno(self, aluno):
+    def _editar_informacoes_aluno(self, aluno): 
+        
         pass
